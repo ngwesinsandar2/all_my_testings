d8adfd5ac082c2a3cd7f44f056f8d0a3bf26e023
Ngwe Sin Sandar <ngwesinsandar112@gmail.com>
2023-05-26 23:31:15 +0630

    added: bootstrap

--- burma_aca/auth/index.php
+++ burma_aca/auth/index.php
@@ -7,29 +7,32 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Home</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 
 <body>
-  <?php if ($_SESSION["logged_in"]) : ?>
-    <h1>Home Page</h1>
-    <h2>
-      <?php echo $_SESSION["user_name"] ?>
-    </h2>
-    <a href="logout.php">Logout</a>
-  <?php else: ?>
-    <form action="has_session.php" method="POST">
-      <div>
-        <label for="user_name">User name</label>
-        <input type="text" name="user_name" id="user_name" />
-      </div>
-      <div>
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" />
-      </div>
-      <button>Login</button>
-    </form>
-  <?php endif; ?>
+  <div class="container">
+    <?php if ($_SESSION["logged_in"]) : ?>
+      <h1>Home Page</h1>
+      <h2>
+        <?php echo $_SESSION["user_name"] ?>
+      </h2>
+      <a href="logout.php" class="btn btn-primary">Logout</a>
+    <?php else : ?>
+      <form action="has_session.php" method="POST">
+        <div class="mb-4">
+          <label for="user_name">User name</label>
+          <input type="text" name="user_name" id="user_name" class="form-control" />
+        </div>
+        <div class="mb-4">
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password" class="form-control" />
+        </div>
+        <button class="btn btn-primary">Login</button>
+      </form>
+    <?php endif; ?>
+  </div>
 </body>
 
 </html>
